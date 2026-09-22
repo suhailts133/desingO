c9fb6910bb54553d590fb8b1ccdfe18fe23364e0
suhailts133 <suhailts2222@gmail.com>
2026-09-22 20:15:26 +0530

    style: removed unnecessary DesignerLayout

--- frontend/src/layouts/DesignerLayout.tsx
+++ frontend/src/layouts/DesignerLayout.tsx
@@ -34,7 +34,7 @@ export default function DesignerLayout() {
       <DesignerSidebar name={name} email={email} />
 
       <div className="flex-1 flex flex-col min-w-0 overflow-hidden">
-        <main className="flex-1 overflow-y-auto p-8 flex justify-center items-start bg-bg">
+        <main className="flex-1 overflow-y-auto p-8 flex justify-center  bg-bg">
           <Outlet />
         </main>
       </div>
